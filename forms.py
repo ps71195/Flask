6570c882df48e83eba1caab37965bd69ddb2f0fc
ps71195 <prashants71195@gmail.com>
2020-06-16 18:19:04 +0530

    sample flask application

--- forms.py
+++ forms.py
@@ -5,5 +5,3 @@ from wtforms.validators import DataRequired, Length, Email
 class Login(FlaskForm):
     username = StringField('Username', validators=[DataRequired(message="input here"), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-
-    submit = SubmitField('Submit')

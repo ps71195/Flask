3213ce680dd09748e4aedb592ad3d2e65779ef14
ps71195 <prashants71195@gmail.com>
2020-06-18 22:55:53 +0530

    p1 branch

--- forms.py
+++ forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, InputRequired
 
 class Login(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(message="input here"), Length(min=2, max=20)])
+    username = StringField('Username', validators=[Length(min=2, max=20, message="not proper")])
     email = StringField('Email', validators=[DataRequired(), Email()])
 
     submit = SubmitField('Submit')
--- hello.py
+++ hello.py
@@ -3,16 +3,17 @@ from forms import Login
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'qwerty'
+dict1 = {'prashant': 'prashants71195@gmail.com'}
 
 
 @app.route("/")
 def home():
     return render_template('home.html')
 
-
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = Login()
+    print(form.username)
     return render_template('login.html', form=form)
 
 

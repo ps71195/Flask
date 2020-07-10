8177a7d1de695e56c0e23a59b2db5ae809932dbe
ps71195 <prashants71195@gmail.com>
2020-07-10 13:35:02 +0530

    stashed changes

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, InputRequired
 
+#hello
 class Login(FlaskForm):
     username = StringField('Username', validators=[Length(min=2, max=20, message="not proper")])
     email = StringField('Email', validators=[DataRequired(), Email()])

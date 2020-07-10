2431505cb352f637ab7846e07bbea1db8699df3b
ps71195 <prashants71195@gmail.com>
2020-07-10 16:31:33 +0530

    done

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length, Email, InputRequired
 
 #hello
 class Login(FlaskForm):
-    username = StringField('Username', validators=[Length(min=2, max=20, message="not proper")])
+    username = StringField('Username', validators=[Length(min=2, max=30, message="not proper")])
     email = StringField('Email', validators=[DataRequired(), Email()])
 
     submit = SubmitField('Submit')
